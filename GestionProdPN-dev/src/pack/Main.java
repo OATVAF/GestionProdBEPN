3c212b6189efff5c46604b979ec1d64e9770084b
S.Goletto <sygoletto@airfrance.fr>
2012-02-26 16:21:34 +0100

    Main temporaire de test ObjdectDB

--- GestionProdPN-dev/src/pack/Main.java
+++ GestionProdPN-dev/src/pack/Main.java
@@ -1,9 +1,21 @@
 package pack;
 
+import java.util.List;
+
+import javax.persistence.*;
+
+import ui.Config;
+import data.DB;
+import data.Formateur;
+import data.Import;
+import data.Stage;
+import data.Module;
+
 /**
  * classe Main (debut du programme)
  * @author BERON Jean-Sébastien
  */
+
 public class Main {
 
 	/**
@@ -12,7 +24,24 @@ public class Main {
 	public static void main(String[] args) {
 		
 		//construction de la fenetre principale
-		new FenetrePrincipale();
+		//new FenetrePrincipale();
+		
+		Config.Read();
+		DB.Open();
+		System.out.println(Config.get("app.version"));
+		
+		DB.Purge();
+		Import.ImportDelia();
+		try {
+			Thread.sleep(1000);
+		} catch (InterruptedException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		Import.ImportPNC();
+		
+		DB.Close();
+		Config.Write();
 		
 	}//fin main()
 
