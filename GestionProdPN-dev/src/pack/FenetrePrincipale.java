a34a00b0a0f9c1d9efac7d5967402327630b7e84
S.Goletto <m330415@airfrance.fr>
2012-03-09 19:12:39 +0100

    Incomplete - task : Mettre la version du build dans la fenêtre principale - v094

--- GestionProdPN-dev/src/pack/FenetrePrincipale.java
+++ GestionProdPN-dev/src/pack/FenetrePrincipale.java
@@ -125,7 +125,7 @@ public class FenetrePrincipale extends JFrame implements ActionListener{
 		titleLabel.setFont(new Font("arial", 1, 30));
 		headerPane.add(titleLabel,BorderLayout.CENTER);
 		
-		versLabel = new JLabel("v093");
+		versLabel = new JLabel("v094");
 		versLabel.setHorizontalAlignment(SwingConstants.RIGHT);
 		versLabel.setVerticalAlignment(SwingConstants.TOP);
 		versLabel.setForeground(Color.BLUE);
